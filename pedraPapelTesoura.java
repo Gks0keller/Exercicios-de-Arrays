bb6d4884dfe4ec2cece0cd835441f93ed1d8b185
GABRIEL KOEHLER DA SILVA <gabriel_k_da-silva@estudante.sesisenai.org.br>
2022-10-31 16:33:09 -0300

    atulização do arquivo

--- pedraPapelTesoura.java
+++ pedraPapelTesoura.java
@@ -8,50 +8,60 @@ public class pedraPapelTesoura {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int escolha, escolhaM;
+		int escolha, escolhaM,m=0,e=0;
 
-		for (String continuar = "s"; continuar != "n"; continuar = continuar) {
+		for (;(m<3 && e<3);) {
 
 			System.out.println("\n  0 representa pedra\n  1 representa papel\n  2 representa tesoura");
 
-			System.out.println("Qual é sua escolha: ");
+			System.out.println(" Qual é sua escolha: ");
 			escolha = sc.nextInt();
 
-			System.out.print("Qual é a escolha da Maquina: \n");
+			System.out.print(" Qual é a escolha da Maquina: \n");
 			escolhaM = r.nextInt(2);
-			System.out.print(escolhaM);
+			System.out.println(escolhaM);
 
 			if (escolha == 0 && escolhaM == 1) {
 
-				System.out.println("\nO vencedor é a Maquina!");
-
+				System.out.println(" Ele ganhou.");
+				m++;
+				
 			} else if (escolha == 1 && escolhaM == 0) {
 
-				System.out.println("\nO vencedor é Você!");
-
+				System.out.println(" Eu ganhei.");
+				e++;
+				
 			} else if (escolha == 1 && escolhaM == 2) {
-
-				System.out.println("\nO vencedor é a Maquina!");
-
+				
+				System.out.println(" Ele ganhou.");
+				m++;
+				
 			} else if (escolha == 2 && escolhaM == 1) {
-
-				System.out.println("\nO vencedor é Você!");
+				
+				System.out.println(" Eu ganhei.");
+				e++;
 
 			} else if (escolha == 2 && escolhaM == 0) {
 
-				System.out.println("\nO vencedor é a Maquina!");
-
+				System.out.println(" Ele ganhou.");
+				m++;
+				
 			} else if (escolha == 0 && escolhaM == 2) {
-
-				System.out.println("\nO vencedor é Você!");
-
+				
+				System.out.println(" Eu ganhei.");
+				e++;
+				
 			} else {
 				System.out.println("\nEmpate");
 			}
+			
 
-			System.out.print("deseja continuar?");
-			continuar = sc.next();
-
+		}
+		
+		if(m>e) {
+			System.out.println(" Maquina Ganhou!!");
+		} else {
+			System.out.println(" Você Ganhou!!");
 		}
 
 	}
